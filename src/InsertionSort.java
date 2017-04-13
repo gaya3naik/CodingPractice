ef93e7d825494082776b93940942555b6a67d415
Gayathri N <gaya3naik@gmail.com>
2017-04-13 23:38:30 +0530

    Insertion Sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -6,15 +6,15 @@ public class InsertionSort {
 
     private void insertionSort(int array[]){
 
-        for(int i =1; i<array.length;i++){
+        for(int i = 1; i<array.length;i++){
 
             int element = array[i];
             int j = i;
-            while( j > 0 && array[j-1] > element){
-                array[j] = array[j-1];
-                j = j-1;
-                array[j] = element;
+            while( j > 0 && array[j-1] > element) {
+                array[j] = array[j - 1];
+                j = j - 1;
             }
+                array[j] = element;
         }
         for(int i =0; i<array.length; i++){
             System.out.print(" "+array[i]);
